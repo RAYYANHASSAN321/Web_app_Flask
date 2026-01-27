2fb091b43088be6d6937393aa75b3f5f41363bde
RAYYANHASSAN321 <rayyanhassan1688@gmail.com>
2026-01-27 10:50:47 +0500

    new page

--- app.py
+++ app.py
@@ -15,5 +15,9 @@ def home():
 def index():
     return render_template('index.html')
 
+@app.route("/contact")
+def contact ():
+    return render_template('contact.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
