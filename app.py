813bc2b9a19796c8c6a43a4599b0fbde9acb4c8c
RAYYANHASSAN321 <rayyanhassan1688@gmail.com>
2026-02-01 22:17:28 +0500

    todo app

--- app.py
+++ app.py
@@ -1,23 +1,53 @@
-from flask import Flask ,  render_template , url_for
+from flask import Flask ,  render_template , url_for ,redirect , request
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# Database setup  
+# client = MongoClient('mongodb://localhost:27017/') # local MongoDB server
+load_dotenv()  # .env load karega
 
-@app.route("/home")
-def home():
-    return "Welcome to home"
+MONGO_URI = os.getenv("MONGO_URI")
+DB_NAME = os.getenv("DB_NAME")
+
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+todo_tbl = db['todo_tbl']
+
+
+
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+
+# @app.route("/home")
+# def home():
+#     return "Welcome to home"
 
 
 @app.route("/index")
 def index():
     return render_template('index.html')
 
+
+
 @app.route("/contact")
 def contact ():
     return render_template('contact.html')
+
+@app.route("/add" , methods = ['POST'])
+def add_todo():
+    title = request.form.get('title')
+    desc = request.form.get('desc')
+    todo_tbl.insert_one({
+        'title': title , 
+        'desc': desc
+        })
+    return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
